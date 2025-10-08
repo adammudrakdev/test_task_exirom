6f30c34e256be9a6bc95d43dedb11bf9a71bf8fd
AdamMudrak <adam.mudrak@exirom.com>
2025-10-08 23:21:27 +0300

    Initial commit

---
+++ src/main/kotlin/com/example/test_task_exirom/TestTaskExiromApplication.kt
@@ -0,0 +1,11 @@
+package com.example.test_task_exirom
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TestTaskExiromApplication
+
+fun main(args: Array<String>) {
+    runApplication<TestTaskExiromApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/test_task_exirom/TestTaskExiromApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.test_task_exirom
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TestTaskExiromApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
