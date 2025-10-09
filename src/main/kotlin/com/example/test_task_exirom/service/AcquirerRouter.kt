6a1e0db01fa1583bf024b18913901f85ba293e28
AdamMudrak <adam.mudrak@exirom.com>
2025-10-09 04:07:43 +0300

    Improve router

--- src/main/kotlin/com/example/test_task_exirom/service/AcquirerRouter.kt
+++ src/main/kotlin/com/example/test_task_exirom/service/AcquirerRouter.kt
@@ -1,14 +1,20 @@
 package com.example.test_task_exirom.service
 
 import com.example.test_task_exirom.dto.TransactionDto
+import com.example.test_task_exirom.exception.CardValidationException
 import org.springframework.stereotype.Component
+import java.util.regex.Pattern
+
+const val REDUCE_CEILING = 9
 
 @Component
 class AcquirerRouter(val privatBankPaymentServiceImpl: PaymentService, val otpBankPaymentServiceImpl: PaymentService) {
+
     fun routeToAcquirer(transactionDto: TransactionDto) {
+        LuhnAlgoUtil.validateCardNumberToLuhn(transactionDto.cardNumber)
         val binCardNumber = transactionDto.cardNumber.take(6)
         var sum = 0
-        for (digit in 0 until binCardNumber.length - 1) {
+        for (digit in 0 ..< binCardNumber.length) {
             sum += (binCardNumber[digit]).digitToInt()
         }
         if (sum / 2 == 0) privatBankPaymentServiceImpl.processPayment(transactionDto)
