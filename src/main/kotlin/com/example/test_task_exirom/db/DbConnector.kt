20edc0718139b083a71b786e3de121bdc56a14e3
AdamMudrak <adam.mudrak@exirom.com>
2025-10-09 19:00:57 +0300

    Add merchants db

--- src/main/kotlin/com/example/test_task_exirom/db/DbConnector.kt
+++ src/main/kotlin/com/example/test_task_exirom/db/DbConnector.kt
@@ -1,8 +1,11 @@
 package com.example.test_task_exirom.db
 
 import com.example.test_task_exirom.entity.Transaction
+import java.util.concurrent.atomic.AtomicLong
 
 object DbConnector {
     val database: MutableMap<Long, Transaction> = LinkedHashMap()
-    var nextTransactionId = 1L
+    var currentTransactionId = AtomicLong(0)
+
+    val merchantDatabase: List<String> = listOf("1qW", "2eR", "3tY", "4uI", "5oP")
 }
