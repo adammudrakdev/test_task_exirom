fd69ba5001be4017e4c01f7ff8cca96c786935e8
AdamMudrak <adam.mudrak@exirom.com>
2025-10-09 14:34:49 +0300

    Rewrite Repo more to kotlin style

--- src/main/kotlin/com/example/test_task_exirom/repository/TransactionRepository.kt
+++ src/main/kotlin/com/example/test_task_exirom/repository/TransactionRepository.kt
@@ -1,44 +1,32 @@
 package com.example.test_task_exirom.repository
 
+import com.example.test_task_exirom.db.DbConnector
 import com.example.test_task_exirom.entity.Transaction
-import com.example.test_task_exirom.exception.NoTransactionException
+import com.example.test_task_exirom.exception.TransactionNotFoundException
 import org.springframework.stereotype.Repository
 import java.lang.Exception
 
 @Repository
 class TransactionRepository {
-    val database: Map<Long, Transaction> = LinkedHashMap()
-    var transactionId = 1L
 
     fun save(transaction: Transaction) {
-        transaction.transactionId = transactionId++
-        database.plus(Pair(transaction.transactionId, transaction))
+        transaction.transactionId = DbConnector.nextTransactionId++
+        DbConnector.database[transaction.transactionId] = (transaction)
     }
 
     fun getById(id: Long): Transaction {
         try {
-            return database[id]!!
+            return DbConnector.database[id]!!
         } catch (_: Exception) {
-            throw NoTransactionException("No transaction with id $id was found!")
+            throw TransactionNotFoundException("No transaction with id $id was found!")
         }
     }
 
     fun getAll() : List<Transaction> {
-        val transactionList = ArrayList<Transaction>()
-        for (key in database.keys) {
-            transactionList.plus(database[key])
-        }
-        return transactionList
+        return DbConnector.database.values.toList()
     }
 
     fun getAllByMerchantId(id: Long) : List<Transaction> {
-        val transactionList = ArrayList<Transaction>()
-        for (key in database.keys) {
-            val transaction: Transaction = database[key]!!
-            if (transaction.merchantId == id) {
-                transactionList.plus(transaction)
-            }
-        }
-        return transactionList
+        return DbConnector.database.values.filter { t -> id == t.merchantId }.toList()
     }
 }
