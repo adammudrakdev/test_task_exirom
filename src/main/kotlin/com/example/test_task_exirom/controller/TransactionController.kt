612dd565a4f7b783d1ba2cf5589138feaf803502
AdamMudrak <adam.mudrak@exirom.com>
2025-10-09 12:44:53 +0300

    Finish controller

--- src/main/kotlin/com/example/test_task_exirom/controller/TransactionController.kt
+++ src/main/kotlin/com/example/test_task_exirom/controller/TransactionController.kt
@@ -1,20 +1,29 @@
 package com.example.test_task_exirom.controller
 
+import com.example.test_task_exirom.dto.GetTransactionDto
 import com.example.test_task_exirom.dto.TransactionDto
 import com.example.test_task_exirom.service.AcquirerRouter
+import com.example.test_task_exirom.service.TransactionService
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.ResponseBody
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-class TransactionController(val acquirerRouter: AcquirerRouter) {
+class TransactionController(val transactionService: TransactionService) {
     @PostMapping
-    fun processTransaction(transactionDto: TransactionDto) {
-        acquirerRouter.routeToAcquirer(transactionDto)
+    fun processTransaction(transactionDto: TransactionDto): GetTransactionDto {
+        return transactionService.routeTransaction(transactionDto)
+    }
+
+    @GetMapping("/{id}")
+    fun getTransactionById(@PathVariable id: Long): GetTransactionDto {
+        return transactionService.getTransactionById(id)
     }
 
     @GetMapping
-    fun getTransactionById(id: Long) {
-
+    fun getAllTransactions(): List<GetTransactionDto> {
+        return transactionService.getAllTransactions()
     }
 }
