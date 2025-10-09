ac5ff11706521ca7244e393d5f9e440906cec38a
AdamMudrak <adam.mudrak@exirom.com>
2025-10-09 18:33:32 +0300

    Change CreateDto

--- src/main/kotlin/com/example/test_task_exirom/dto/CreateTransactionDto.kt
+++ src/main/kotlin/com/example/test_task_exirom/dto/CreateTransactionDto.kt
@@ -2,45 +2,40 @@ package com.example.test_task_exirom.dto
 
 import com.example.test_task_exirom.enum.Currency
 import com.example.test_task_exirom.validation.ValidDate
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import io.swagger.v3.oas.annotations.media.Schema
 import jakarta.validation.constraints.Digits
-import jakarta.validation.constraints.Max
 import jakarta.validation.constraints.NotBlank
-import jakarta.validation.constraints.NotNull
 import jakarta.validation.constraints.Pattern
 
 const val CARD_DIGITS_AND_SIZE_REGEX = "\\d{16}"
 const val CVV_DIGITS_AND_SIZE_REGEX = "\\d{3}"
-const val DATE_REGEX = "^(0[1-9]|1[0-2])/[0-9]{2}$\n"
+const val MERCHANT_ID_DIGITS_AND_SIZE_REGEX = "\\d{1,32}"
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 data class TransactionDto(
     @field:Schema(name = "cardNumber", example = "0000000000000000", required = true)
     @field:NotBlank
-    @field:Pattern(regexp = CARD_DIGITS_AND_SIZE_REGEX)
+    @field:Pattern(regexp = CARD_DIGITS_AND_SIZE_REGEX, message = "cardNumber: must match pattern 0000000000000000")
     val cardNumber: String,
 
     @field:Schema(name = "expiryDate", example = "01/26", required = true)
     @field:NotBlank
-    @field:Pattern(regexp = DATE_REGEX)
     @field:ValidDate
     val expiryDate: String,
 
     @field:Schema(name = "cvv", example = "000", required = true)
     @field:NotBlank
-    @field:Pattern(regexp = CVV_DIGITS_AND_SIZE_REGEX)
+    @field:Pattern(regexp = CVV_DIGITS_AND_SIZE_REGEX, message = "cvv: must match pattern 000")
     val cvv: String,
 
-    @field:Schema(name = "amount", example = "0.1", required = true)
-    @field:NotNull
-    @field:Digits(integer = 7, fraction = 2)
-    @field:Max(1000000)
+    @field:Schema(name = "amount", example = "125", required = true)
+    @field:Digits(integer = 7, fraction = 2, message = "amount: must match pattern 999999.99")
     val amount: Double,
 
-    @field:Schema(oneOf = [Currency::class],name = "currency", example = "USD", required = true)
-    @field:NotNull
+    @field:Schema(oneOf = [Currency::class], name = "currency", example = "USD", required = true)
     val currency: Currency,
 
     @field:Schema(name = "merchantId", example = "123456789", required = true)
-    @field:NotNull
-    @field:Digits(integer = 17, fraction = 0)
-    val merchantId: Long)
+    @field:Pattern(regexp = MERCHANT_ID_DIGITS_AND_SIZE_REGEX, message = "merchantId: must match pattern 0, 000, ..., 00000000000000000000000000000000")
+    val merchantId: String)
