99f65702b7302e995ed4f8466250552135b04415
AdamMudrak <adam.mudrak@exirom.com>
2025-10-10 02:16:48 +0300

    Add separate date mapper, move from validator

--- src/main/kotlin/com/example/test_task_exirom/validation/ValidDateValidator.kt
+++ src/main/kotlin/com/example/test_task_exirom/validation/ValidDateValidator.kt
@@ -1,9 +1,9 @@
 package com.example.test_task_exirom.validation
 
+import com.example.test_task_exirom.service.utils.DateMapperUtil
 import jakarta.validation.ConstraintValidator
 import jakarta.validation.ConstraintValidatorContext
 import java.time.LocalDate
-import java.time.YearMonth
 import java.util.regex.Pattern
 
 const val BASE_YEAR = 2000
@@ -12,12 +12,6 @@ const val DATE_REGEX = "^(0[1-9]|1[0-2])/[0-9]{2}$"
 class ValidDateValidator : ConstraintValidator<ValidDate, String> {
 
     override fun isValid(expiryDate: String, context: ConstraintValidatorContext?): Boolean {
-        return Pattern.matches(DATE_REGEX, expiryDate) && LocalDate.now().isBefore(parseExpiryDate(expiryDate))
-    }
-
-    private fun parseExpiryDate(expiryDate: String): LocalDate {
-        val month: Int = if (expiryDate[0] == '0') expiryDate[1].digitToInt() else ("${expiryDate[0]}${expiryDate[1]}").toInt()
-        val year: Int = BASE_YEAR + ("${expiryDate[3]}${expiryDate[4]}").toInt()
-        return YearMonth.of(year, month).atEndOfMonth()
+        return Pattern.matches(DATE_REGEX, expiryDate) && LocalDate.now().isBefore(DateMapperUtil.toLocalDate(expiryDate))
     }
 }
