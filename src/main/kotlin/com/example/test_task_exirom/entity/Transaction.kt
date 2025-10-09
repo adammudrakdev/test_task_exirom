e26880999fb0620904e951c148b91924ca70a3d4
AdamMudrak <adam.mudrak@exirom.com>
2025-10-09 03:55:46 +0300

    Add toString(), delete changeStatus() as irrelevant

--- src/main/kotlin/com/example/test_task_exirom/entity/Transaction.kt
+++ src/main/kotlin/com/example/test_task_exirom/entity/Transaction.kt
@@ -3,9 +3,11 @@ package com.example.test_task_exirom.entity
 import com.example.test_task_exirom.enum.Currency
 import com.example.test_task_exirom.enum.TransactionStatus
 
-class Transaction(val transactionId: String, val cardNumber: String, val cvv: String, val amount: Double, val currency: Currency,
+class Transaction(var transactionId: Long = 0L, val cardNumber: String, val cvv: String, val amount: Double, val currency: Currency,
                   val merchantId: String, var status: TransactionStatus = TransactionStatus.PENDING) {
-    fun changeStatus(status: TransactionStatus) {
-        this.status = status
+
+    override fun toString(): String {
+        return "Transaction(transactionId=$transactionId, cardNumber='$cardNumber', cvv='$cvv', amount=$amount, " +
+                "currency=$currency, merchantId='$merchantId', status=$status)"
     }
 }
