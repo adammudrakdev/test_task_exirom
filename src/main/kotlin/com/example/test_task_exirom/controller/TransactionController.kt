d5b8af09bc4e6a7e6d9744e6d562c448b260fe99
AdamMudrak <adam.mudrak@exirom.com>
2025-10-09 14:57:23 +0300

    Add request body and mapping

--- src/main/kotlin/com/example/test_task_exirom/controller/TransactionController.kt
+++ src/main/kotlin/com/example/test_task_exirom/controller/TransactionController.kt
@@ -6,12 +6,15 @@ import com.example.test_task_exirom.service.TransactionService
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
-@RestController("/transactions")
+@RestController
+@RequestMapping("/transactions")
 class TransactionController(val transactionService: TransactionService) {
     @PostMapping
-    fun processTransaction(transactionDto: TransactionDto): GetTransactionDto {
+    fun processTransaction(@RequestBody transactionDto: TransactionDto): GetTransactionDto {
         return transactionService.routeTransaction(transactionDto)
     }
 
