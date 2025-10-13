498a3136d1d8363129265cccf28e480990fd4ea8
AdamMudrak <adam.mudrak@exirom.com>
2025-10-13 15:54:40 +0300

    Validate multiple errors on controller level instead of one

--- src/main/kotlin/com/example/test_task_exirom/exception/ExceptionControllerAdvice.kt
+++ src/main/kotlin/com/example/test_task_exirom/exception/ExceptionControllerAdvice.kt
@@ -61,11 +61,11 @@ class ExceptionControllerAdvice {
     }
 
     @ExceptionHandler
-    fun handleMethodArgumentNotValidException(ex: MethodArgumentNotValidException): ResponseEntity<ErrorMessageDto> {
-        val errorMessage = ErrorMessageDto(
-            HttpStatus.BAD_REQUEST.value(),
-            ex.fieldError!!.defaultMessage!!
-        )
-        return ResponseEntity(errorMessage, HttpStatus.BAD_REQUEST)
+    fun handleMethodArgumentNotValidException(ex: MethodArgumentNotValidException): ResponseEntity<Map<String, Any>> {
+        val errorsMap: MutableMap<String, Any> = LinkedHashMap()
+        errorsMap["status"] = HttpStatus.BAD_REQUEST
+        val errorMessagesList: List<String?> = ex.bindingResult.allErrors.map { error -> error.defaultMessage }.toList()
+        errorsMap["errors"] = errorMessagesList
+        return ResponseEntity(errorsMap, HttpStatus.BAD_REQUEST)
     }
 }
