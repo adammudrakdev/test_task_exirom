08911cd0934652eb57e5d2c52281558ee7dea1f5
AdamMudrak <adam.mudrak@exirom.com>
2025-10-16 21:42:43 +0300

    Use constants

--- src/main/kotlin/com/example/test_task_exirom/component/transaction/mapper/DateMapperUtil.kt
+++ src/main/kotlin/com/example/test_task_exirom/component/transaction/mapper/DateMapperUtil.kt
@@ -1,23 +1,36 @@
 package com.example.test_task_exirom.component.transaction.mapper
 
-import com.example.test_task_exirom.component.transaction.validation.BASE_YEAR
+import com.example.test_task_exirom.common.Constants.BASE_YEAR
+import com.example.test_task_exirom.common.Constants.EXPIRY_DATE_THIRD_CHAR_DELIMITER
+import com.example.test_task_exirom.common.Constants.EXPIRY_MONTH_FIRST_DIGIT_INDEX
+import com.example.test_task_exirom.common.Constants.EXPIRY_MONTH_SECOND_DIGIT_INDEX
+import com.example.test_task_exirom.common.Constants.EXPIRY_YEAR_FIRST_DIGIT_INDEX
+import com.example.test_task_exirom.common.Constants.EXPIRY_YEAR_SECOND_DIGIT_INDEX
+import com.example.test_task_exirom.common.Constants.MONTH_PREFIX_FOR_SINGLE_DIGIT
+import com.example.test_task_exirom.common.Constants.OCTOBER_MONTH_NUMBER
+import com.example.test_task_exirom.common.Constants.YEAR_STRING_LAST_TWO_DIGITS_INDEX
 import java.time.LocalDate
 import java.time.YearMonth
 
-const val EXPIRY_DATE_DELIMITER = "/"
-
 object DateMapperUtil {
     fun toLocalDate(expiryDate: String): LocalDate {
-        val month: Int = if (expiryDate[0] == '0') expiryDate[1].digitToInt() else ("${expiryDate[0]}${expiryDate[1]}").toInt()
-        val year: Int = BASE_YEAR + ("${expiryDate[3]}${expiryDate[4]}").toInt()
+        val month: Int =
+            if (expiryDate[EXPIRY_MONTH_FIRST_DIGIT_INDEX] == MONTH_PREFIX_FOR_SINGLE_DIGIT) {
+                expiryDate[EXPIRY_MONTH_SECOND_DIGIT_INDEX].digitToInt()
+            } else {
+                ("${expiryDate[EXPIRY_MONTH_FIRST_DIGIT_INDEX]}${expiryDate[EXPIRY_MONTH_SECOND_DIGIT_INDEX]}").toInt()
+            }
+        val year: Int = BASE_YEAR + ("${expiryDate[EXPIRY_YEAR_FIRST_DIGIT_INDEX]}${expiryDate[EXPIRY_YEAR_SECOND_DIGIT_INDEX]}").toInt()
+
         return YearMonth.of(year, month).atEndOfMonth()
     }
 
     fun toExpiryDateDto(expiryDate: LocalDate): String {
         val month = expiryDate.month.ordinal
-        val year = expiryDate.year.toString().substring(2)
-        return StringBuilder().append(if (month < 10) "0".plus(month) else month)
-            .append(EXPIRY_DATE_DELIMITER)
+        val year = expiryDate.year.toString().substring(YEAR_STRING_LAST_TWO_DIGITS_INDEX)
+
+        return StringBuilder().append(if (month < OCTOBER_MONTH_NUMBER) MONTH_PREFIX_FOR_SINGLE_DIGIT.plus(month) else month)
+            .append(EXPIRY_DATE_THIRD_CHAR_DELIMITER)
             .append(year).toString()
     }
 }
