0520796453d0dc84bb58f70211ef8fbe25e0f498
AdamMudrak <adam.mudrak@exirom.com>
2025-10-16 21:42:57 +0300

    Separate logic of ID from DbConnector

--- src/main/kotlin/com/example/test_task_exirom/infra/db/DbConnector.kt
+++ src/main/kotlin/com/example/test_task_exirom/infra/db/DbConnector.kt
@@ -1,11 +1,9 @@
 package com.example.test_task_exirom.infra.db
 
 import com.example.test_task_exirom.component.transaction.model.Transaction
-import java.util.concurrent.atomic.AtomicLong
 
 object DbConnector {
     val database: MutableMap<Long, Transaction> = LinkedHashMap()
-    var currentTransactionId = AtomicLong(0)
 
     val merchantDatabase: List<String> = listOf("1qW", "2eR", "3tY", "4uI", "5oP")
 }
