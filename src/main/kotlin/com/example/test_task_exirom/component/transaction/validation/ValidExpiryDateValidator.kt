a3ccb7ec847fc76429d36772e80beec73317f18f
AdamMudrak <adam.mudrak@exirom.com>
2025-10-16 21:48:02 +0300

    Move constants to constants object

--- src/main/kotlin/com/example/test_task_exirom/component/transaction/validation/ValidExpiryDateValidator.kt
+++ src/main/kotlin/com/example/test_task_exirom/component/transaction/validation/ValidExpiryDateValidator.kt
@@ -1,13 +1,11 @@
 package com.example.test_task_exirom.component.transaction.validation
 
+import com.example.test_task_exirom.common.Constants.DATE_REGEX
 import com.example.test_task_exirom.component.transaction.mapper.DateMapperUtil
 import jakarta.validation.ConstraintValidator
 import jakarta.validation.ConstraintValidatorContext
 import java.time.LocalDate
 import java.util.regex.Pattern
-
-const val BASE_YEAR = 2000
-const val DATE_REGEX = "^(0[1-9]|1[0-2])/[0-9]{2}$"
 
 class ValidDateValidator : ConstraintValidator<ValidExpiryDate, String> {
 
