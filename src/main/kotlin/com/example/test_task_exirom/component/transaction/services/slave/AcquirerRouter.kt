868e03a14808615b66c0776476143605df8e77ab
AdamMudrak <adam.mudrak@exirom.com>
2025-10-16 21:42:27 +0300

    Use list intstead of specific constructor dependencies, use FP, use utils

--- src/main/kotlin/com/example/test_task_exirom/component/transaction/services/slave/AcquirerRouter.kt
+++ src/main/kotlin/com/example/test_task_exirom/component/transaction/services/slave/AcquirerRouter.kt
@@ -1,26 +1,26 @@
 package com.example.test_task_exirom.component.transaction.services.slave
 
+import com.example.test_task_exirom.common.Constants.ACQUIRER_A
+import com.example.test_task_exirom.common.Constants.ACQUIRER_B
+import com.example.test_task_exirom.common.Utils.isEven
 import com.example.test_task_exirom.component.transaction.model.Transaction
 import com.example.test_task_exirom.component.transaction.services.slave.acquirer.Acquirer
 import org.springframework.stereotype.Component
 
 @Component
-class AcquirerRouter(val acquirerA: Acquirer, val acquirerB: Acquirer) {
+class AcquirerRouter(val acquirers: List<Acquirer>) {
 
     fun routeToAcquirer(transaction: Transaction): Acquirer {
-        return when (verifyBinSum(transaction)) {
-            true -> acquirerA
-            else -> acquirerB
-        }
+        return when (checkBinNumberSumEven(transaction)) {
+            true -> acquirers.find { it.javaClass.simpleName.equals(ACQUIRER_A) }
+            else -> acquirers.find { it.javaClass.simpleName.equals(ACQUIRER_B) }
+        }!!
     }
 
-    private fun verifyBinSum(transaction: Transaction): Boolean {
+    private fun checkBinNumberSumEven(transaction: Transaction): Boolean {
         val binCardNumber = transaction.cardNumber.take(6)
-        var sum = 0
-        for (digit in 0 ..< binCardNumber.length) {
-            sum += (binCardNumber[digit]).digitToInt()
-        }
+        val sum: Int = binCardNumber.map { it.digitToInt() }.sum()
 
-        return sum % 2 == 0
+        return sum.isEven()
     }
 }
